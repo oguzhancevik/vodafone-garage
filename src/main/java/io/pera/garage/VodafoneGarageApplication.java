3cece349dbf2892e48d01e30b71492ab4347d666
Oguzhan Cevik <cevik.ogsan@gmail.com>
2022-03-13 00:11:45 +0300

    initial commit

---
+++ src/main/java/io/pera/garage/VodafoneGarageApplication.java
@@ -0,0 +1,13 @@
+package io.pera.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneGarageApplication.class, args);
+	}
+
+}
